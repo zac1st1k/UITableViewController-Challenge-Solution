48a2fc16711ad2ebbd21ce2eff0cf28e9c02fb1e
zac1st1k <zacfirst@gmail.com>
2014-12-07 20:13:02 +1100

    + Data source

--- UITableVIewController Challenge Solution/XZZUITableViewController.m
+++ UITableVIewController Challenge Solution/XZZUITableViewController.m
@@ -46,26 +46,41 @@
 {
 #warning Potentially incomplete method implementation.
     // Return the number of sections.
-    return 0;
+    return 3;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
 #warning Incomplete method implementation.
     // Return the number of rows in the section.
-    return 0;
+    if (section == 0) {
+        return 2;
+    }
+    else if (section == 1){
+        return 1;
+    }
+    else{
+    return 3;
+    }
 }
 
-/*
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
-    
+    NSString *Cellidentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:Cellidentifier forIndexPath:indexPath];
+ 
     // Configure the cell...
-    
+    if (indexPath.section == 0) {
+        cell.textLabel.text = @"I am in section 0";
+    }
+    else if (indexPath.section == 1){
+        cell.textLabel.text = @"Another section";
+    }
+    else{
+        cell.textLabel.text = [NSString stringWithFormat:@"Cell %i", indexPath.row];
+    }
     return cell;
 }
-*/
 
 /*
 // Override to support conditional editing of the table view.
